368a41eaea2834850a724109819150e2d1772408
Mickael158 <119030927+Mickael158@users.noreply.github.com>
2023-07-03 21:52:34 +0300

    Add files via upload

--- Framework/src/etu1904/framework/servlet/FrontServlet.java
+++ Framework/src/etu1904/framework/servlet/FrontServlet.java
@@ -23,6 +23,7 @@ public class FrontServlet extends HttpServlet {
     HashMap<String, Mapping> mappingUrls;
     HashMap<String, Object> singleton;
     private Util util;
+    private String sessionVariable;
 
     @Override
     public void init() throws ServletException {
@@ -33,8 +34,12 @@ public class FrontServlet extends HttpServlet {
             this.mappingUrls = new HashMap<>();
             this.singleton = new HashMap<>();
             final String tomPath = "/WEB-INF/classes/";
+            this.sessionVariable = getInitParameter("session");
+
             String path = getServletContext().getRealPath(tomPath);
             util.loadMapping(path, tomPath, mappingUrls, singleton);
+
+            System.out.println(this.sessionVariable);
 
         } catch (Exception e) {
             throw new RuntimeException(e);
@@ -60,7 +65,7 @@ public class FrontServlet extends HttpServlet {
 
             if (map == null) throw new Exception("Not Found");
 
-            ModelView mv = util.invokeMethod(request, map, singleton);
+            ModelView mv = util.invokeMethod(request, map, singleton, sessionVariable);
             util.setAttributeRequest(request, mv);
             request.getRequestDispatcher(mv.getView()).forward(request, response);
 
