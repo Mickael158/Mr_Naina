11524f3a04b153724f64ffc88688f8f4bbaa8ffb
Mickael158 <119030927+Mickael158@users.noreply.github.com>
2023-07-03 21:38:14 +0300

    Delete TestFramework directory

--- TestFramework/script.bat
+++
@@ -1,9 +0,0 @@
-cd "D:\ITU\Web Dyn\Framework\out\production\Framework\"
-jar cvf fw.jar *
-copy "./fw.jar" "D:\ITU\Web Dyn\TestFramework\WEB-INF\lib\"
-cd "D:\ITU\Web Dyn\TestFramework\WEB-INF\classes"
-javac -cp "D:\ITU\Web Dyn\TestFramework\WEB-INF\lib\fw.jar" -d . *.java
-cd "D:\ITU\Web Dyn\TestFramework"
-jar cvf "TestFramework.war" *
-copy "TestFramework.war" "C:\Program Files\Apache Software Foundation\Tomcat 10.1\webapps\"
-del "TestFramework.war"
